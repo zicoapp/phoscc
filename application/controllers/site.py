57a620908359cdc038f6958e515fc8ce30e84093
tinyao <yao2app@gmail.com>
2015-12-01 16:52:19 +0800

    some fix

--- application/controllers/site.py
+++ application/controllers/site.py
@@ -293,8 +293,8 @@ def hot(cat=None):
 @bp.route('/_load', methods=['POST'])
 def load():
     page = int(request.form['page'])
-    if 'category' in category:
-        catid = request.form['category']
+    catid = request.form['category']
+    if catid:
         category = Query(Category).get(catid)
         query_by_cat = Relation.reverse_query('Photo', 'category', category)
         photos = query_by_cat.skip((page-1) * 24).limit(24).find()
